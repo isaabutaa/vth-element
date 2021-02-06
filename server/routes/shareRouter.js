134b01f892e281937b4995439331acbb300934b5
Isa Abutaa <isaabutaa@gmail.com>
2021-02-06 17:54:32 -0500

    created crud routes in shareRouter

--- server/routes/shareRouter.js
+++ server/routes/shareRouter.js
@@ -2,16 +2,71 @@ const express = require('express')
 const shareRouter = express.Router()
 const Share = require('../models/share.js')
 
+// get all shares & post one
+shareRouter.route("/")
+    .get((req, res, next) => {
+        Share.find((err, shares) => {
+            if(err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(shares)
+        })
+    })
+    .post((req, res, next) => {
+        req.body.user = req.user._id
+        const newShare = new Share(req.body)
+        newShare.save((err, savedShare) => {
+            if(err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(savedShare)
+        })
+    })
 
-// get all posts
+// get shares by user
+shareRouter.get("/userShares", (req, res, next) => {
+    Share.find(
+        { user: req.user._id },
+        (err, shares) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(shares)
+        }
+    )
+})
 
-// post one
-
-// edit post
-
-// delete post
-
-
+// edit & delete
+shareRouter.route("/:shareId")
+    .put((req, res, next) => {
+        Share.findOneAndUpdate(
+            { _id: req.params.shareId, user: req.user._id },
+            req.body,
+            { new: true },
+            (err, updatedShare) => {
+                if(err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(201).send(updatedShare)
+            }
+        )
+    })
+    .delete((req, res, next) => {
+        Share.findOneAndDelete(
+            { _id: req.params.shareId, user: req.user._id },
+            (err, deletedShare) => {
+                if(err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(200).send(`Deleted ${deletedShare.shareText.slice(0, deletedShare.shareText.length / 2)}...`)
+            }
+        )
+    })
 
 
 module.exports = shareRouter
