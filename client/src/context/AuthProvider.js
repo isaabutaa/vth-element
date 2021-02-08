fb38a6bf74421dbd8be8d926069b468c479d9f8d
Isa Abutaa <isaabutaa@gmail.com>
2021-02-08 08:03:34 -0500

    saved token and username in localStorage. Also wrote signout function to remove localStorage and reset state

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -2,14 +2,28 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 export const authContext = React.createContext() 
+export const userAxios = axios.create()
+
+userAxios.interceptors.request.use(config => {
+    const token = localStorage.getItem("token")
+    config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+const initUserState = {
+    user: JSON.parse(localStorage.getItem) || {},
+    token: localStorage.getItem("token") || ""
+}
 
 export default function AuthProvider(props) {
-    const [userState, setUserState] = useState({ user: {}, token: ""})
+    const [userState, setUserState] = useState(initUserState)
 
     function login(credentials) {
         axios.post("/auth/login", credentials)
             .then(res => {
                 const { user, token } = res.data
+                localStorage.setItem("user", JSON.stringify(user))
+                localStorage.setItem("token", token)
                 setUserState({ user, token })
             })
             .catch(err => console.error(err.response.data.errMsg))
@@ -19,16 +33,26 @@ export default function AuthProvider(props) {
         axios.post("/auth/signup", credentials)
             .then(res => {
                 const { user, token } = res.data
+                localStorage.setItem("user", JSON.stringify(user))
+                localStorage.setItem("token", token)
                 setUserState({ user, token })
             })
             .catch(err => console.error(err.response.data.errMsg))
     }
+
+    function signout() {
+        localStorage.removeItem("user")
+        localStorage.removeItem("token")
+        setUserState({ user: {}, token: "" })
+    }
+    
     return (
         <authContext.Provider 
             value={{
                 ...userState,
                 login,
-                signup
+                signup,
+                signout
             }}
         >
             {props.children}
