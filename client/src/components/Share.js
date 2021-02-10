78d05fc50483b2aec01f2f5d89b77c8c6413b215
Isa Abutaa <isaabutaa@gmail.com>
2021-02-10 17:14:50 -0500

    some styling for edit & delete

--- client/src/components/Share.js
+++ client/src/components/Share.js
@@ -1,10 +1,13 @@
 
 export default function Share(props) {
     const {shareText, _id, datePosted, likes, user}= props
+    const date = datePosted.slice(0, datePosted.indexOf('T'))
     return (
         <div className="share">
-            <h4>{shareText}</h4>
-            <p>shared: {datePosted}</p>
+            <h5>{shareText}</h5>
+            <p>shared: {date}</p>
+            <p className="edit">edit</p>
+            <p className="delete">delete</p>
         </div>
     )
 }
