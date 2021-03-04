b90e7b988a1101af3d9f17edf977ba6139097ae1
Isa Abutaa <isaabutaa@gmail.com>
2021-03-04 16:40:30 -0500

    user login bug

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -28,28 +28,30 @@ authRouter.post("/signup", (req, res, next) => {
 
 // login
 authRouter.post("/login", (req, res, next) => {
-    User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
-        if(err) {
-            res.status(500)
-            return next(err)
-        }
-        if(!user) {
-            res.status(403)
-            return next(new Error("Incorrect username or password"))
-        }
-        user.verifyPassword(req.body.password, (err, isMatch) => {
+    User.findOne(
+        { username: req.body.username.toLowerCase() }, 
+        (err, user) => {
             if(err) {
                 res.status(500)
                 return next(err)
             }
-            if(!isMatch) {
-                res.status(403)
-                return next(new Error("Username or Password is incorrect"))
+            if(!user) {
+                res.status(500)
+                return next(new Error("Check: Incorrect username or password"))
             }
-            const token = jwt.sign(user.hidePassword(), process.env.MY_SECRET)
-            return res.status(200).send({ token, user: user.hidePassword() })
+            user.verifyPassword(req.body.password, (err, isMatch) => {
+                if(err) {
+                    res.status(500)
+                    return next(err)
+                }
+                if(!isMatch) {
+                    res.status(403)
+                    return next(new Error("Username or Password is incorrect"))
+                }
+                const token = jwt.sign(user.hidePassword(), process.env.MY_SECRET)
+                return res.status(200).send({ token, user: user.hidePassword() })
+            })
         })
-    })
 })
 
 module.exports = authRouter
